86354d877a01eecd8824848e0b48ae486c222696
Anders Haakonsen <anhakonsen@gmail.com>
2021-01-16 15:49:34 +0100

    5. Intro to Styled Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
-
+//GlobalStyle
+import GlobalStye from "./components/GlobalStyle";
 function App() {
   return (
     <div className="App">
+      <GlobalStye />
       <AboutUs />
     </div>
   );
