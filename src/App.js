1841b16edabf2bc024a1cb960721fec6b777beff
Anders Haakonsen <anhakonsen@gmail.com>
2021-01-16 18:04:49 +0100

    Added our work, and react router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 //GlobalStyle
 import GlobalStye from "./components/GlobalStyle";
+import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+// Router
+import { Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <GlobalStye />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
